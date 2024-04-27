f6a7bd93fff08078664cfaf15ae94109699cc1d0
Kelly Norton <kellegous@gmail.com>
2024-04-26 20:50:20 -0400

    comments

--- bin/allow_list.php
+++ bin/allow_list.php
@@ -2,6 +2,33 @@
 
 declare(strict_types=1);
 
+/******************************************************************************
+ *  Execution:    java Allowlist allowlist.txt < data.txt
+ *  Execution:    php bin/allow_list.php allowList.txt < data.txt
+ *
+ *  Data files:   https://algs4.cs.princeton.edu/11model/tinyAllowlist.txt
+ *                https://algs4.cs.princeton.edu/11model/tinyText.txt
+ *                https://algs4.cs.princeton.edu/11model/largeAllowlist.txt
+ *                https://algs4.cs.princeton.edu/11model/largeText.txt
+ *
+ *  Allowlist filter.
+ *
+ *  % php bin/allow_list.php tinyAllowlist.txt < tinyText.txt
+ *  50
+ *  99
+ *  13
+ *
+ *  % php bin/allow_list.php largeAllowList.txt < largeText.txt | more
+ *  499569
+ *  984875
+ *  295754
+ *  207807
+ *  140925
+ *  161828
+ *  [367,966 total values]
+ *
+ ******************************************************************************/
+
 use Kellegous\Algs4\In;
 use Kellegous\Algs4\StaticSetOfInts;
 use Kellegous\Algs4\Stdio;
