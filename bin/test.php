fb822af62266700ae25e50656c0ab3dd75067aab
Kelly Norton <kellegous@gmail.com>
2024-05-03 19:28:44 -0400

    finished Interval2D

--- bin/test.php
+++ bin/test.php
@@ -9,12 +9,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $m = new Drawing(800, 600);
 $m->floodFill(400, 300, $m->colorFromRGB(0xff, 0xff, 0xff));
-$m->ellipse(400, 300, 200, 150)
-    ->fill($m->colorFromRGBA(0, 0, 0, 200));
+$m->ellipse(400, 300, 1, 1)
+    ->fill($m->colorFromRGB(0, 0, 0));
 $m->writePNG(Destination::toFile('test.png'));
 
-$a = $m->colorFromRGB(0xff, 0xff, 0xff);
-var_dump($a->alpha());
-printf("%x\n", $a->getID());
-$b = $m->colorFromRGBA(0xff, 0xff, 0xff, 127);
-printf("alpha = %x\n", $b->alpha());
--- src/Commands/Interval2D.php
+++ src/Commands/Interval2D.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Kellegous\Algs4\Commands;
 
+use InvalidArgumentException;
 use Kellegous\Algs4\Counter;
+use Kellegous\Algs4\Graphics\Canvas;
+use Kellegous\Algs4\Graphics\Color;
+use Kellegous\Algs4\Graphics\Destination;
+use Kellegous\Algs4\Graphics\Drawing;
 use Kellegous\Algs4\Interval1D;
 use Kellegous\Algs4\Point2D;
 use Kellegous\Algs4\Random;
@@ -15,6 +20,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use function Kellegous\Algs4\parse_float;
@@ -26,10 +32,21 @@ use function Kellegous\Algs4\parse_int;
 )]
 final class Interval2D extends Command
 {
+    private const int SIZE = 512;
+
+    private const string DEFAULT_DEST = 'interval2d.png';
 
     #[Override]
     protected function configure(): void
     {
+        $this->addOption(
+            'dest',
+            'd',
+            InputOption::VALUE_REQUIRED,
+            'the destination file',
+            self::DEFAULT_DEST
+        );
+
         $this->addArgument(
             'xmin',
             InputArgument::REQUIRED,
@@ -62,16 +79,24 @@ final class Interval2D extends Command
         InputInterface $input,
         OutputInterface $output
     ): int {
-        $x_min = parse_float($input->getArgument('xmin'));
-        $x_max = parse_float($input->getArgument('xmax'));
-        $y_min = parse_float($input->getArgument('ymin'));
-        $y_max = parse_float($input->getArgument('ymax'));
+        $x_min = self::getFloatArgument($input, 'xmin');
+        $x_max = self::getFloatArgument($input, 'xmax');
+        $y_min = self::getFloatArgument($input, 'ymin');
+        $y_max = self::getFloatArgument($input, 'ymax');
         $trials = parse_int($input->getArgument('trials'));
+
+        $drawing = new Drawing(self::SIZE, self::SIZE);
+        $canvas = new Canvas($drawing);
 
         $x_interval = Interval1D::fromMinMax($x_min, $x_max);
         $y_interval = Interval1D::fromMinMax($y_min, $y_max);
         $box = \Kellegous\Algs4\Interval2D::fromXY($x_interval, $y_interval);
-        // $box->draw();
+        $canvas->rectangle(
+            $box->x()->min(),
+            $box->y()->min(),
+            $box->x()->max(),
+            $box->y()->max()
+        )->stroke(Color::black($drawing));
 
         $counter = new Counter('hits');
         $random = Random::withEngine(new Mt19937());
@@ -81,12 +106,33 @@ final class Interval2D extends Command
             $point = new Point2D($x, $y);
             if ($box->contains($point)) {
                 $counter->increment();
+            } else {
+                $canvas->point($x, $y, Color::black($drawing));
             }
-            // else $point->draw();
         }
 
         Stdio::out()->println($counter);
         Stdio::out()->printf("box area = %.2f\n", $box->area());
+
+        $drawing->writePNG(
+            Destination::toFile($input->getOption('dest'))
+        );
+
         return Command::SUCCESS;
+    }
+
+    private static function getFloatArgument(
+        InputInterface $input,
+        string $name,
+        float $min = 0.0,
+        float $max = 1.0
+    ): float {
+        $v = parse_float($input->getArgument($name));
+        if ($v < $min || $v > $max) {
+            throw new InvalidArgumentException(
+                sprintf('%s must be in range (%0.2g, %0.2g)', $name, $min, $max)
+            );
+        }
+        return $v;
     }
 }
